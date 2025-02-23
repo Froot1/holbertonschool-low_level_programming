d20d7fc7f6368045ad6a8af2ce96f97535af72ad
root <10679@holbertonstudents.com>
2025-02-23 22:02:30 +0000

    101-keygen.c

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,58 +2,38 @@
  * File: 101-keygen.c
  * Auth: Fahad Alonazi
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - Generates random valid passwords for the
- *        program 101-crackme.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+        int i = 0, sum = 0, a;
 
-	srand(time(0));
+	srand(time(NULL));
 
-	while (sum < 2772)
+        while (1)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
+		a = rand()%100;
+		sum += a;
 
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-
-		for (index = 0; password[index]; index++)
+		if (sum <= 2772)
 		{
-			if (password[index] >= (33 + diff_half1))
+		        printf("%c", a);
+
+			if (sum == 2772)
 			{
-				password[index] -= diff_half1;
 				break;
 			}
 		}
-		for (index = 0; password[index]; index++)
+		else
 		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
+			sum -= a;
+			i--;
 		}
-	}
 
-	printf("%s", password);
+		i++;
+	}
 
 	return (0);
 }
