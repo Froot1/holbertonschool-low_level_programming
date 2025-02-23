4cdf8da1235c2c07d9ace9211c37f854734acce0
root <10679@holbertonstudents.com>
2025-02-23 17:54:46 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,40 +2,43 @@
  * File: 7-puts_half.c
  * Auth: Fahad Alonazi
  */
+
 #include "main.h"
- /**
-  * puts_half - function that prints half of a string
-  * followed by a new line
-  *
-  * @str: parameter defined in main
-  *
-  * Return: void
-  */
+
+/**
+ * puts_half - function that prints half of a string
+ * followed by a new line
+ *
+ * @str: parameter defined in main
+ *
+ * Return: void
+ */
+
 void puts_half(char *str)
 {
-int i = 0;
-int j;
-int l;
+	int i = 0;
+	int j;
+	int l;
 
-while (str[i] != '\0')
-{
-i++;
-}
-l = i;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	l = i;
 
-if (l % 2 == 0)
-{
-for (j = l / 2; j < l; j++)
-{
-_putchar(str[j]);
-}
-}
-else
-{
-for (j = (l + 1) / 2; j < l; j++)
-{
-_putchar(str[j]);
-}
-}
-_putchar('\n');
+	if (l % 2 == 0)
+	{
+		for (j = l / 2; j < l; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for (j = (l + 1) / 2; j < l; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
 }
