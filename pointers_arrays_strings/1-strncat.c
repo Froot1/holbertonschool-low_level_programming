8bbe8c1ab9d5d07a81b889d450522d3839e015c6
root <10679@holbertonstudents.com>
2025-02-24 16:17:28 +0000

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,32 @@
 /*
- * File: 1-_strncat.c
  * Auth: Fahad Alonazi
  */
+#include "main.h"
 
- #include "main.h"
-
- /**
-  * _strncat - concatenate two strings
-  * @dest: string
-  * @src: string
-  * @n: number of elements to concatenate in
-  * Return: pointer to resulting `dest`
-  */
+/**
+ * _strncat - Write a function that concatenates two strings. it will use at
+ *            most n bytes from src
+ *
+ * @dest: This is the output  dest
+ * @src: This is the input src
+ * @n: This is the number bytes fro src
+ *
+ * Return: This is my return
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, c;
-for (i = 0; dest[i] != '\0'; i++)
-;
-for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
-{
-dest[i] = src[c];
-}
-return (dest);
+	int index, count;
+
+	for (index = 0; dest[index] != '\0'; index++)
+		;
+	for (count = 0; count < n && src[count] != '\0'; count++)
+	{
+		dest[index] = src[count];
+		index++;
+	}
+	if (count < n)
+	{
+	dest[index] = '\0';
+	}
+	return (dest);
 }
