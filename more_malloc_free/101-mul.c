09377b542ff680ee431783ce91f72e3b47d1ca30
root <10679@holbertonstudents.com>
2025-03-12 19:09:06 +0000

    101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -8,95 +8,112 @@
 #include <string.h>
 
 /**
- * larger_num - get larger num
- * @n1: n1
- * @n2: n2
- * Return: larger index.
+ * _isdigit - checks if character is digit
+ * @c: the character to check
+ *
+ * Return: 1 if digit, 0 otherwise
  */
-int larger_num(char *n1, char *n2)
+int _isdigit(int c)
 {
-	int l1, l2;
-
-	l1 = strlen(n1);
-	l2 = strlen(n2);
-	/*missing cases like 001 19*/
-	if (l1 > l2)
-		return (1);
-	else if (l1 < l2)
-		return (2);
-	else if ((*n1 == '\0') && (*n2 == '\0'))
-	{
-		return (1);
-	}
-	else if ((*n1 != '\0') && (*n2 != '\0'))
-	{
-		if (*n1 >= *n2)
-			return (1);
-		if (*n1 < *n2)
-			return (2);
-	}
-	return ((larger_num(n1++, n2++)));
+	return (c >= '0' && c <= '9');
 }
+
 /**
- * is_number - is it number
- * @n: n1
- * Return: 1 ture or 0 false
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
  */
-int is_number(char *n)
+int _strlen(char *s)
 {
-	while (*n)
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ *
+ * Return: the product big number string
+ */
+char *big_multiply(char *s1, char *s2)
+{
+	char *r;
+	int l1, l2, a, b, c, x;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
 	{
-		if (!((*n >= '0') && (*n <= '9')))
-			return (0);
-		n++;
+		if (!_isdigit(s1[l1]))
+		{
+			free(r);
+			printf("Error\n"), exit(98);
+		}
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+
+			c /= 10;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
 	}
-	return (1);
+	return (r);
 }
-/**
- * mul_number - get larger num
- * @n1: n1
- * @n2: n2
- * Return: larger index.
- */
-char *mul_number(char *n1, char *n2)
-{
-	char **v;
 
-	(void) v;
-	(void) n1;
-	(void) n2;
-	return (NULL);
-}
-/**
- * main - multiplies two numbers "arguments"
- * @argc: argument count
- * @argv: argument vector
- * Return: (0) success or (1) faild
- */
-int main(int argc, char *argv[])
-{
-	int larger, isNumbers;
-	char *total;
 
-	(void) total;
+/**
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char **argv)
+{
+	char *r;
+	int a, c, x;
+
 	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	isNumbers = is_number(argv[1]) +  is_number(argv[1]);
-	if (isNumbers != 2)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	larger = larger_num(argv[1], argv[2]);
+		printf("Error\n"), exit(98);
 
-	if (larger == 1)
-		total = mul_number(argv[1], argv[2]);
-	else
-		total = mul_number(argv[2], argv[1]);
-
-	printf("%lu\n", (atol(argv[1]) * atol(argv[2])));
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
+	{
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(r);
 	return (0);
 }
