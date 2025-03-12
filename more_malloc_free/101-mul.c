c76fc541e9842c7d46c20aa477641f39fcff12bc
root <10679@holbertonstudents.com>
2025-03-12 19:28:41 +0000

    101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -2,118 +2,111 @@
  * File: 101-_mul.c
  * Auth: Fahad Alonazi
  */
+
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
+ * is_positive_number - Checks if a string represents a positive number
+ * @str: The string to check
  *
- * Return: 1 if digit, 0 otherwise
+ * Return: 1 if the string is a positive number, 0 otherwise
  */
-int _isdigit(int c)
+int is_positive_number(char *str)
 {
-	return (c >= '0' && c <= '9');
-}
+	if (!str || *str == '\0')
+		return (0);
 
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
- *
- * Return: the product big number string
- */
-char *big_multiply(char *s1, char *s2)
-{
-	char *r;
-	int l1, l2, a, b, c, x;
-
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
-
-	for (l1--; l1 >= 0; l1--)
+	while (*str)
 	{
-		if (!_isdigit(s1[l1]))
-		{
-			free(r);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
+		if (!isdigit(*str))
+			return (0);
+		str++;
 	}
-	return (r);
+
+	return (1);
 }
 
+/**
+ * multiply_strings - Multiplies two large positive numbers as strings
+ * @num1: First number as string
+ * @num2: Second number as string
+ *
+ * Return: The result as a dynamically allocated string
+ */
+char *multiply_strings(char *num1, char *num2)
+{
+	int len1 = strlen(num1), len2 = strlen(num2);
+	int *result = calloc(len1 + len2, sizeof(int));
+	char *final_result;
+	int i, j, product;
+
+	if (!result)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			product = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1];
+			result[i + j + 1] = product % 10;
+			result[i + j] += product / 10;
+		}
+	}
+
+	final_result = malloc(len1 + len2 + 1);
+	if (!final_result)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	i = 0;
+	while (i < len1 + len2 && result[i] == 0)
+		i++;
+
+	j = 0;
+	while (i < len1 + len2)
+		final_result[j++] = result[i++] + '0';
+
+	final_result[j] = '\0';
+	free(result);
+
+	return (*final_result ? final_result : strdup("0"));
+}
 
 /**
- * main - multiply two big number strings
- * @argc: the number of arguments
- * @argv: the argument vector
+ * main - Multiplies two positive numbers passed as arguments
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: Always 0 on success.
+ * Return: 0 on success, 98 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *r;
-	int a, c, x;
+	char *result;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		printf("Error\n");
+		return (98);
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(r);
+
+	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	result = multiply_strings(argv[1], argv[2]);
+	printf("%s\n", result);
+	free(result);
+
 	return (0);
 }
