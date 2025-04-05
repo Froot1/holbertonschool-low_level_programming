c9d9113db384090b2de0e73f685778d19675877c
root <10679@holbertonstudents.com>
2025-04-05 04:16:04 +0000

    100-first.c

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+/*
+ * File: 100-first.c
+ * Auth: Fahad Alonazi
+ */
+
+#include "lists.h"
+
+/**
+* first - Print before main prints
+*
+* Return: void
+*/
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
