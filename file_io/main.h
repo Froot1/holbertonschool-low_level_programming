2f9ffb22e26dfbe34b5d8b2bb745770fec5fb532
root <10679@holbertonstudents.com>
2025-04-05 13:20:30 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -11,4 +11,11 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+
+/* Function prototypes */
+void check_args(int argc);
+void open_files(char **argv, int *fd_from, int *fd_to);
+void copy_content(int fd_from, int fd_to, char **argv);
+void close_files(int fd_from, int fd_to);
+int main(int argc, char **argv);
 #endif
