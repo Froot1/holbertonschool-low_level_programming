015b2e84b96e0845a72a2e0cfe99adf357a65219
root <10679@holbertonstudents.com>
2025-04-05 12:05:27 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,15 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
+/* Includes */
+
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define BUFSIZE 1204
+#include <unistd.h>
+#include <stdio.h>
+
+/* Structures */
+
+
+/* Prototypes */
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
-#endif
+int copy_f(int from, const char *f_from, int to, const char *f_to);
+
+#endif /* _HOLBERTON_H_ */
