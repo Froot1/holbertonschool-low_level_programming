855c681c149da1a46b3cf870e7a4d02fa5dff2f3
root <10679@holbertonstudents.com>
2025-04-05 11:55:51 +0000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,84 +1,42 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - program that copies the content of a file to another.
- * @argc: number of input arguments.
- * @argv: input arguments array.
- *
- * Return: 0 on success.
- */
-
-int main(int argc, char *argv[])
+  * main - entry point
+  * @ac: argument count
+  * @av: array of argument tokens
+  * Return: 0 on success
+  */
+int main(int ac, char *av[])
 {
-	int from, to, error = 0;
+	int fd_from, fd_to, rd_stat, wr_stat;
+	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	char buffer[BUFSIZE];
 
-	if (argc != 3)
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	rd_stat = 1;
+	while (rd_stat)
 	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
-		exit(97);
-	}
-
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	error = copy_f(from, argv[1], to, argv[2]);
-	if (error != 0)
-		exit(error);
-
-	if (close(from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
-	if (close(to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
-	return (0);
-}
-
-/**
- * copy_f - executes the copy of the files validated on main.
- * @from: file descriptor number from which the content will be taken.
- * @f_from: name of the file from.
- * @to: file descriptor number where the copy will be made.
- * @f_to: name of the file to.
- *
- * Return: 0 on success, 98 if fails to read from, 99 if fails to write to.
- */
-
-int copy_f(int from, const char *f_from, int to, const char *f_to)
-{
-	char buf[1024] = "";
-	ssize_t bsize = sizeof(buf) / sizeof(buf[0]);
-	ssize_t r = 1, w = 0;
-
-	while ((r = read(from, buf, bsize)) > 0)
-	{
-		w = write(to, buf, r);
-		if (w != r)
+		rd_stat = read(fd_from, buffer, BUFSIZE);
+		if (rd_stat == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		if (rd_stat > 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_to);
-			exit(99);
+			wr_stat = write(fd_to, buffer, rd_stat);
+			if (wr_stat != rd_stat || wr_stat == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
-	if (r < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_from);
-		return (98);
-	}
-
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
+
