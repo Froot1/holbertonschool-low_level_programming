3f3e5697a8e98ae788066dc1932f92d9af5ef888
root <10679@holbertonstudents.com>
2025-04-05 13:27:56 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,21 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
 
-/* Function prototypes */
-void check_args(int argc);
-void open_files(char **argv, int *fd_from, int *fd_to);
-void copy_content(int fd_from, int fd_to, char **argv);
-void close_files(int fd_from, int fd_to);
-int main(int argc, char **argv);
-#endif
+#endif 
